bfbb86604d69b2e09cb5c16c516759afa2737594
Daniel <spyker156@gmail.com>
2022-02-14 18:52:14 +0200

    Update notifications (add stacking notifications, styles change)

--- src/Web/Workshop.Web/wwwroot/js/notification.js
+++ src/Web/Workshop.Web/wwwroot/js/notification.js
@@ -9,18 +9,32 @@ notifyConnection.start().catch(function (err) {
 
 notifyConnection.on("NewNotification",
     function (message) {
-        var toastLiveExample = document.getElementById('liveToast');
-        var toast = new bootstrap.Toast(toastLiveExample);
-        $("#notifyMessage").empty();
-        $("#notificationSender").empty();
-        $("#notificationLink").val('');
-        $("#notificationLink").val(message.link);
-        $("#notifyMessage").append(message.message);
-        $("#notificationSender").append(message.sender);
-        toast.show();
-    });
+        var count = $('.toast').length + 1;
+        var toastId = 'toast' + count;
+        if (count > 5) {
+            var toastToRemove = document.getElementsByClassName('toast')[0].getAttribute('id');
+            console.log(toastToRemove);
+            $("#" + toastToRemove).remove();
+        }
 
-$("#notification").click(function () {
-    var link = $("#notificationLink").val();
-    window.location = link;
-});
+        var miliseconds = 10 * 1000;
+
+        document.getElementById("notifications").innerHTML = document.getElementById("notifications").innerHTML +
+            ` <div id="${toastId}" class="toast border-all-borders-gold" role="alert" aria-live="assertive" aria-atomic="true" data-delay="${miliseconds}" style="background-color: goldenrod !important;">
+              <div class="toast-header" style="background-color: #5D6166 !important">
+              <img src="/siteLogo.jpg" class="rounded mr-2" alt="/siteLogo.jpg" width="20" height="20">
+              <strong class="mr-auto text-green pl-1 pr-5">${message.sender}</strong>
+              <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+              <span aria-hidden="true" class="text-warning">&times;</span>
+              </button>
+              </div>
+              <a href="${message.link}">
+              <div class="toast-body text-warning" style="background-color: #5D6166 !important">
+              ${message.message}
+              </div>
+              </div>
+              </a>`;
+
+        $('.toast').toast('show').then(setTimeout(function () { $("#" + toastId).remove(); }, miliseconds));
+    }
+);
